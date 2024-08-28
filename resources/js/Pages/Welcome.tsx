62f16ee46e42d2c3ec5d8355944142c901e69693
sustainboostci <serge.touvoli@inphb.ci>
2024-08-28 23:40:07 +0000

    To public

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -13,8 +13,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
         <>
             <Guest>
             <Head title="Home" />
-            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-                {/* <img id="background" className="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" /> */}
+            {/* <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <div className="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                     <div className="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                         <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
@@ -156,7 +155,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                         </footer>
                     </div>
                 </div>
-            </div>
+            </div> */}
             </Guest>
         </>
     );
