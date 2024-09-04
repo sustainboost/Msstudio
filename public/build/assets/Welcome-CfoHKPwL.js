4e4ce709b97b2737166ff343c165e408d64ee53a
sustainboostci <serge.touvoli@inphb.ci>
2024-09-04 01:26:15 +0000

    deployfirst

---
+++ public/build/assets/Welcome-CfoHKPwL.js
@@ -0,0 +1,1 @@
+import{j as e,Y as r}from"./app-CuJD1cPi.js";import{G as t}from"./GuestLayout-BTiYVWtO.js";function m({auth:o,laravelVersion:s,phpVersion:i}){return e.jsx(e.Fragment,{children:e.jsx(t,{children:e.jsx(r,{title:"Home"})})})}export{m as default};
